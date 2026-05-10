43f4d8e451108927cd8a8d71eac04e5988b1ca85
Stefano Nirwana <stefano.budi96@smk.belajar.id>
2026-05-10 14:41:52 +0700

    chore: remove unused Composable

--- app/src/main/java/dev/stefano/learnmvvm/MainActivity.kt
+++ app/src/main/java/dev/stefano/learnmvvm/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dev.stefano.learnmvvm.mvvm.CounterView
 import dev.stefano.learnmvvm.ui.theme.LearnMVVMTheme
 
@@ -20,21 +16,5 @@ class MainActivity : ComponentActivity() {
                 CounterView()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LearnMVVMTheme {
-        Greeting("Android")
     }
 }

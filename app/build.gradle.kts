c0936d48d91886e23a334e5d8a010740ece47bad
Stefano Nirwana <stefano.budi96@smk.belajar.id>
2026-05-11 11:15:20 +0700

    chore: add suppress warning targetSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,7 @@ android {
     defaultConfig {
         applicationId = "dev.stefano.learnmvvm"
         minSdk = 24
+        //noinspection OldTargetApi
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"

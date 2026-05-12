66adea72423d41e52d5ce3cbb6856b8649507c15
Stefano Nirwana <stefano.budi96@smk.belajar.id>
2026-05-12 14:58:11 +0700

    chore: add hilt navigation compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.dagger.hilt.android)
+    implementation(libs.hilt.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
